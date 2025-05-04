2841a651e2fc1b5a2b8333fd2a7b945f97287c28
BautistaAliende <bau.joaquin@gmail.com>
2025-05-04 10:08:26 -0300

    Update Fuego.java

--- src/clases/pokemon/Fuego.java
+++ src/clases/pokemon/Fuego.java
@@ -10,16 +10,27 @@ public class Fuego extends Pokemon {
 	}
 	
 	// "Heredaciones" -----------------------------------------------------------------
-    
+
+	/**Metodo para atacar un pokemon<br>
+	 * 
+	 * <b>Precondicion:</b> El parametro "adversario" debe ser distinto de null
+	 * 
+	 */
+
 	public void atacar(Pokemon adversario) {
-    	double nuevoAtaque;
+    		double nuevoAtaque;
    	 
-    	adversario.recibeDano(this.getAtaque()/5);
-    	nuevoAtaque = this.getAtaque()*3/4;
-    	if (nuevoAtaque>10)
-        	this.setAtaque(nuevoAtaque);
+	    	adversario.recibeDano(this.getAtaque()/5);
+	    	nuevoAtaque = this.getAtaque()*3/4;
+	    	if (nuevoAtaque>10)
+	        	this.setAtaque(nuevoAtaque);
 	}
-	
+
+	/**Metodo para recibir daño<br>
+	 * 
+	 * <b>Precondicion:</b> El parametro "dano" debe ser > 0
+	 * 
+	 */
 	public void recibeDano(double dano) {
     	if (this.getEscudo()>0) {
     		dano = dano/4;
@@ -34,26 +45,34 @@ public class Fuego extends Pokemon {
 	}
 	
 	public void recargar() {
-    	this.setEscudo(escudoPD*(80 + 5*this.getCategoria())/100);
-    	this.setAtaque(ataquePD*(80 + 5*this.getCategoria())/100);
-    	this.setVida(vidaPD*(80 + 5*this.getCategoria())/100);
+	    	this.setEscudo(escudoPD*(80 + 5*this.getCategoria())/100);
+	    	this.setAtaque(ataquePD*(80 + 5*this.getCategoria())/100);
+	    	this.setVida(vidaPD*(80 + 5*this.getCategoria())/100);
 	}
     
 	public void hechizadoNiebla() {
-    	this.setAtaque(this.getAtaque()/2);
+    		this.setAtaque(this.getAtaque()/2);
 	}
+	
 	public void hechizadoViento() {
-    	this.setVida(this.getVida()/2);
+    		this.setVida(this.getVida()/2);
 	}
+	
 	public void hechizadoTormenta() {
-    	this.setEscudo(this.getEscudo()*0.8);
-    	this.setAtaque(this.getAtaque()*0.8);
+	    	this.setEscudo(this.getEscudo()*0.8);
+	    	this.setAtaque(this.getAtaque()*0.8);
 	}
     
 	public double getCosto() {
     	return costo;
 	}
-	
+
+	/**Metodo que se encarga de clonar al pokemon Fuego<br>
+	 * 
+	 * NUNCA SON clonable)<br>
+	 *
+	 * @throws CloneNotSupportedException 
+	 */
 	@Override
 	public Object clone() throws CloneNotSupportedException {
 		throw new CloneNotSupportedException("Los Pokemones tipo fuego no pueden clonarse");
